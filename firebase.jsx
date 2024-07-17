1331841e0cc0bf3929e3fb162c596e910eecd36d
Boris Kizenko <boriskizenko@wc-dhcp49d203.student-secure.wireless.fsu.edu>
2024-07-17 10:53:49 -0400

    connected to firebase

---
+++ firebase.jsx
@@ -0,0 +1,19 @@
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9sYhslQn18WpnbunFE_6T-Uo4Spb-lzA",
+  authDomain: "groceryscraper-a7b4e.firebaseapp.com",
+  projectId: "groceryscraper-a7b4e",
+  storageBucket: "groceryscraper-a7b4e.appspot.com",
+  messagingSenderId: "112621586657",
+  appId: "1:112621586657:web:f68a34dc9dc8e00b920fa0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
